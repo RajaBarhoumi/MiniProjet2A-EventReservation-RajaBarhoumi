f9c6f208b401cad7a744e879f2f7ebaeb89c6e5b
RajaBarhoumi <rajabarhoumi355@gmail.com>
2026-03-25 19:26:36 +0100

    feat: create custom RefreshToken entity to force database schema synchronization

---
+++ src/Entity/RefreshToken.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'refresh_tokens')]
+class RefreshToken extends BaseRefreshToken
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    protected $id;
+
+    #[ORM\Column(type: 'string', length: 128, unique: true)]
+    protected $refreshToken;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    protected $username;
+
+    #[ORM\Column(type: 'datetime')]
+    protected $valid;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
