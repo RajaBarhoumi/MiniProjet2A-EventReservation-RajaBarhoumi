84331cefcaccd20b446f3cbaa393a42152904108
RajaBarhoumi <rajabarhoumi355@gmail.com>
2026-03-25 18:37:02 +0100

    fix: explicitly initialize refresh token string and expiration in AuthApiController

--- src/Controller/Api/AuthApiController.php
+++ src/Controller/Api/AuthApiController.php
@@ -83,7 +83,9 @@ class AuthApiController extends AbstractController
             $passkeyService->verifyRegistration($credential, $user);
             $jwt = $this->jwtManager->create($user);
             $refresh = $this->refreshManager->create();
+            $refresh->setRefreshToken(); 
             $refresh->setUsername($user->getUserIdentifier());
+            $refresh->setValid((new \DateTime())->modify('+2592000 seconds'));
             $this->refreshManager->save($refresh);
 
             return $this->json([
